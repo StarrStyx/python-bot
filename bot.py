18b9e17cd58a8d5688593a7a4bbef967cafc535d
Adrian <starrstyx@users.noreply.github.com>
2018-10-19 00:00:21 +0100

    yea

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from random import randint
 from random import choice
 
-TOKEN = environ.get['BOT_TOKEN', default=None]
+TOKEN = 'NDk5NjAyOTU1MjgwOTczODI1.Dqqh4Q.kwnFSvFUIsMP73wtPKXkyLHkz74'
 
 client = commands.Bot(command_prefix = '-')
 
