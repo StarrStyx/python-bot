7c62919668797af0bf916911407d05b6b579cf8e
Adrian <33841167+StarrStyx@users.noreply.github.com>
2019-10-29 17:41:26 +0800

    Split off api fetch to other file

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import discord
+import json
+import osu
 
 # bot token & client instance
-token = "aaa"
+token = "var1"
 client = discord.Client()
 bot = commands.Bot(command_prefix='*')
 
 @client.event
 async def on_ready():
-	print('logged on')
+   print('logged on')
 
-
+async def on_message(message):
+    if 0 == 1:
+        osu.get_user()
 
 client.run(token)
---
+++ osu.py
@@ -0,0 +1,12 @@
+﻿import discord
+import json
+import asyncio
+import requests
+
+apikey = "1ff00082bf0c95397f6bb245503be38f3ae7d96e"
+root = "https://osu.ppy.sh/api/get_user?k=" + apikey
+
+def get_user():
+    user = requests.get(root + '&u=starrstyx')
+    print(user.text)
+    return
