65ab224161180696216a4095bde83a9d21bdbced
Adrian <33841167+StarrStyx@users.noreply.github.com>
2019-12-06 15:41:28 +0800

    asdf

--- osu.py
+++ osu.py
@@ -3,7 +3,7 @@ import json
 import asyncio
 import requests
 
-apikey = "1ff00082bf0c95397f6bb245503be38f3ae7d96e"
+apikey = "var1"
 api_url = "https://osu.ppy.sh/api/get_user?k=" + apikey
 
 def get_user(username):
